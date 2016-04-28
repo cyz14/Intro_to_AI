a00b37b13ad9c2770571e187f46a16488ece1ca4
cyz14 <icyz14@163.com>
2016-04-28 12:06:02 +0800

    add some descriptions of GA

--- src/Genetic.cpp
+++ src/Genetic.cpp
@@ -17,6 +17,26 @@
 
 */
 
+/*
+## Outline of the Basic Genetic Algorithm
+// from obitko.com/tutorials/genetic-algorithms/ga-basic-description.php
+
+1. [Start] Generate random population of n chromosomes (suitable solutions for the problem)
+2. [Fitness] Evaluate the fitness f(x) of each chromosome x in the population
+3. [New population] Create a new population by repeating following steps until the new population is complete
+      1. [Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
+      2. [Crossover] With a crossover probability cross over the parents to form a new offspring (children). If no crossover was performed, offspring is an exact copy of parents.
+      3. [Mutation] With a mutation probability mutate new offspring at each locus (position in chromosome).
+      4. [Accepting] Place new offspring in a new population
+4. [Replace] Use new generated population for a further run of algorithm
+5. [Test] If the end condition is satisfied, stop, and return the best solution in current population
+6. [Loop] Go to step 2
+
+
+Making new population only by new offspring can cause lost of the best chromosome from the last population. This is true, so so called elitism is often used. This means, that at least one best solution is copied without changes to a new population, so the best solution found can survive to end of run.
+
+*/
+
 extern const int SIZE = 100;
 extern const double pc = 1.00;
 extern const double pm = 0.01;
@@ -38,11 +58,19 @@ void Genetic::workout(std::ostream & fout) {
   int t = 0;
   initPopulation(colony, SIZE);
 
+  int bestGeneId = 0;
+  double bestFitVal = 0.0;
   double sumFit = 0.0;
   for (size_t i = 0; i < SIZE; ++i) {
     fit[i] = fitvalue(colony[i]);
     sumFit += fit[i];
+
+    if (fit[i] > bestFitVal) {
+      bestFitVal = fit[i];
+      bestGeneId = i;
+    }
   }
+  
   for (size_t i = 0; i < SIZE; i++) {
     prob[i] = fit[i] / sumFit;
   }
@@ -53,6 +81,7 @@ void Genetic::workout(std::ostream & fout) {
   }
 
   // (7)
+
   // (8)
   // (9)
   // (10)
